cae0e58372f584c4efe74bc42bf438ecf2ddaf09
Davit Ghushchyan <Davit.Ghushchyan@Macbooks-MacBook-Pro.local>
2018-12-16 02:12:09 +0400

    Fixed PodIntegration Issue, added DataManager and PersistentController

---
+++ In Yerevan/In Yerevan/DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  In Yerevan
+//
+//  Created by Davit Ghushchyan on 12/16/18.
+//  Copyright © 2018 com.inYerevan. All rights reserved.
+//
+
+import Foundation
---
+++ In Yerevan/In Yerevan/Models/PersistentController.swift
@@ -0,0 +1,18 @@
+//
+//  PersistentController.swift
+//  In Yerevan
+//
+//  Created by Davit Ghushchyan on 12/16/18.
+//  Copyright © 2018 com.inYerevan. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct Persistent {
+    
+    
+    
+    
+    
+}
